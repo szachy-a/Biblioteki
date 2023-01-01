3653ae04b3214362071a4ed4f53e31a0279f5e81
szachy-a <86373033+szachy-a@users.noreply.github.com>
2023-01-01 14:14:24 +0100

    q

---
+++ publikacja do pypi/consoleAnsiInterface/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='consoleAnsiInterface',
+    version='1.0.1',
+    author='szachy-a',
+    description='Console interface with ANSI sequences for Windows',
+    packages=['consoleAnsiInterface'],
+    install_requires=['cursor']
+)
